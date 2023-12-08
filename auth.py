c506f35bece713df79ff195718e7501097aee675
Raditya Azka Prabaswara <93713795+Iyaq1@users.noreply.github.com>
2023-12-08 23:59:58 +0700

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,142 @@
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+import json
+from pydantic import BaseModel
+from passlib.hash import bcrypt
+from typing import Annotated
+import jwt
+from typing import Union
+from passlib.context import CryptContext
+
+oauth2_router = APIRouter()
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class User(BaseModel):
+	id : int
+	username : str
+	admin : bool
+	hashPass : str
+	
+class UserAccountInput(BaseModel):
+    username: str
+    password : str
+    admin : bool
+
+json_filename="user.JSON"
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_hash(password):
+     return pwd_context.hash(password)
+
+def verify_hash_and_password(password, hashpass):
+     return pwd_context.verify(password, hashpass)
+
+
+with open(json_filename,"r") as read_file:
+	userdata = json.load(read_file)
+
+@oauth2_router.post("/users")
+async def add_user(user :UserAccountInput):
+    username_found = False
+    user_id = 0
+
+    for item in userdata:
+        if user_id <= item['id']:
+              user_id = item['id'] + 1
+        if user.username == item['username']:
+            username_found = True
+            raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username is already taken."
+            )
+    if not(username_found) :
+        pass_hash = get_hash(user.password)
+        insert_user = {"id" : user_id, "username" : user.username, "admin" : user.admin, "hashPass" : pass_hash}
+        userdata.append(insert_user)
+        with open(json_filename,"w") as write_file:
+            json.dump(userdata, write_file)
+
+    return "success"
+
+def get_user_from_input(username:str, password:str):
+    userfound = False
+    for item in userdata:
+          if username == item["username"]:
+                if verify_hash_and_password(password, item["hashPass"]):
+                    return item
+                else: #Wrong Password
+                     return False
+    return False
+                    
+def add_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    data_to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data_to_encode.update({"exp": expire})
+    encodedJWT = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encodedJWT
+     
+
+
+@oauth2_router.post("/token", response_model=Token)
+async def create_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = get_user_from_input(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expiring = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = add_access_token(
+    data={"sub": user["username"], "id" : user["id"]}, expires_delta=access_token_expiring
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+async def get_current_user(token : Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        token_decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = token_decoded.get("sub")
+        if username is None:
+            raise credentials_exception
+
+    except jwt.JWTError:
+        raise credentials_exception
+
+    for item in userdata:
+        if item['username'] == username:
+            return item
+    raise credentials_exception
+
+
+# @oauth2_router.delete("/users")
+# async def delete_user(username :str):
+#     username_found = False
+
+#     for idx, item in enumerate(userdata):
+#         if username == item['username']:
+#             username_found = True
+#             userdata.pop(idx)
+#             with open(json_filename,"w") as write_file:
+#                 json.dump(userdata, write_file)
+            
+                 
+#     if not(username_found) :
+#         return "username not found"
+#     return "success"
+
--- main.py
+++ main.py
@@ -1,36 +1,68 @@
 from typing import Annotated
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException, Depends
 import json
 from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
+from auth import oauth2_router as auth_router
+from auth import get_current_user
+from auth import User
 app = FastAPI()
-
+app.include_router(auth_router)
 
 class Questions(BaseModel):
 	id: int
 	question: str
 	correct_answer : str
 	score_weight : int
+
+
 class Answer(BaseModel):
 	id: int
 	answer : str
+	student : int
+	course_id : int
+class Course(BaseModel):
+	id : int
+	owner : int
+	name : str
+	course_id : int
+class AnswerInput(BaseModel):
+	id: int
+	answer : str
+	course_id : int
+class CourseInput(BaseModel):
+	id : int
+	owner : int
+	name : str
 	
 
-json_filename="questions.JSON"
+class QuestionsInput(BaseModel):
+	question: str
+	correct_answer : str
+	score_weight : int
+	course : int
 
-with open(json_filename,"r") as read_file:
-	data = json.load(read_file)
+
+question_filename="questions.JSON"
+answer_filename = "answer.JSON"
+
+with open(question_filename,"r") as read_file:
+	question_data = json.load(read_file)
+
+with open(answer_filename,"r") as read_file:
+	answer_data = json.load(read_file)
 
 @app.get('/')
-async def hello_world():
+async def hello_world(user = Depends(get_current_user)):
 	return {"message": "Hello World"}
 
-@app.get('/questions/{question_id}')
-async def read_questions(question_id: int):
-	print(question_id)
-	for question in data["questions"]:
-		print(question)
-		if  question["id"] == (question_id):
+@app.get('/questions/{course_id}/{question_id}') #reworked
+async def read_questions(question_id: int,course_id : int, user = Depends(get_current_user)):
+	for course in question_data:
+		if course["id"] == course_id:
+			question_data_this = course["question"]
+	for question in question_data_this:
+		if  question['id'] == (question_id):
 			return question
 		
 
@@ -38,22 +70,32 @@ async def read_questions(question_id: int):
 		status_code=404, detail=f'menu not found'
 	)
 
-@app.post('/answer')
-async def add_answer(answer: Answer):
+@app.post('/answer') #reworked
+async def add_answer(answer: AnswerInput, user = Depends(get_current_user)):
 	answer_dict = answer.dict()
 	answer_found = False
-	for answer_list in data['user_answers']:
-		if answer_list['id'] == answer_dict['id']:
+	course_found = False
+	for item in question_data:
+		if item['id'] == answer_dict['course_id']:
+			course_found = True
+			question_data_this = item["question"]
+			break
+	if not(course_found):
+		return "Invalid Course Number"
+	for answer_list in answer_data:
+		if (answer_list['id'] == answer_dict['id']) and (answer_list["student"] == user["id"]) and (answer_list["course_id"] == answer_dict["course_id"]):
 			item_found = True
 			return "Answer "+str(answer_dict['id'])+" exists."
 	if not (answer_found):
 		question_found = False
-		for question_list in data["questions"]:
+		for question_list in question_data_this:
+			
 			if question_list['id'] == answer_dict['id']:
 				question_found = True
-				data['user_answers'].append(answer_dict)
-				with open(json_filename,"w") as write_file:
-					json.dump(data, write_file)
+				answer_input = {"id" : answer.id, "answer" : answer.answer, "student" : user["id"], "course_id" : answer.course_id}
+				answer_data.append(answer_input)
+				with open(answer_filename,"w") as write_file:
+					json.dump(answer_data, write_file)
 
 				return answer_dict  
 		if not (question_found):
@@ -62,55 +104,118 @@ async def add_answer(answer: Answer):
 			status_code=404, detail=f'item not found'
 	)
 
-@app.get('/score')
-async def get_score():
+@app.get('/score/{course_id}') #reworked
+async def get_score(course_id = int,user = Depends(get_current_user)):
+	course_id = int(course_id)
 	score = 0
-	for answer_list in data['user_answers']:
-		for question_list in (data['questions']):
-			if ((answer_list['answer'] == question_list['correct_answer'] ) and (answer_list['id'] == question_list['id']  )):
+	found = False
+	for course in question_data:
+		if course["id"] == course_id:
+			question_data_this = course["question"]
+			found = True
+	if not(found):
+		return "course not found"	
+	for answer_list in answer_data:
+		for question_list in (question_data_this):
+			if ((answer_list['answer'] == question_list['correct_answer'] ) and (answer_list['id'] == question_list['id']  ) and (answer_list["course_id"] == course_id)):
 				score += question_list['score_weight']
 	return score
 	
 
-@app.delete('/answer')
-async def delete_answer():
-
-	for questions_idx, answer_item in enumerate(data['user_answers']):
-		if answer_item['id'] != 0:
-			data['user_answers'].pop(questions_idx)
-			
-			with open(json_filename,"w") as write_file:
-				json.dump(data, write_file)
-	return "updated"
-
+@app.delete('/answer/{course_id}/{question_id}') #reworked
+async def delete_answer(course_id : int, question_id : int,user = Depends(get_current_user)):
+	found = False
+	course_id = int(course_id)
+	question_id = int(question_id)
+	for questions_idx, answer_list in enumerate(answer_data):
+		if (answer_list['id'] == question_id) and (answer_list["student"] == user["id"]) and (answer_list["course_id"] == course_id):
+			answer_data.pop(questions_idx)
+			found = True
+			break
+	if (found):
+		with open(answer_filename,"w") as write_file:
+			json.dump(answer_data, write_file)
+		return "updated"
+	else:
+		return "not found"
 @app.get('/answer')
-async def get_all_answer():
-	return data['user_answers']
+async def get_all_answer(user = Depends(get_current_user)):
+	return answer_data
 
 @app.get('/questions')
-async def get_all_question():
-	return data['questions']
+async def get_all_question(user = Depends(get_current_user)):
+	return question_data
 
-@app.put('/answer')
-async def change_answer(answer :Answer):
+@app.put('/answer/{course_id}/{question_id}') #reworked
+async def change_answer(course_id : int, question_id : int, answer : str, user = Depends(get_current_user)):
+	course_id = int(course_id)
+	question_id = int(question_id)	
+	found = False
+	for questions_idx, answer_list in enumerate(answer_data):
+		if (answer_list['id'] == question_id) and (answer_list["student"] == user["id"]) and (answer_list["course_id"] == course_id):
+			answer_data[questions_idx]["answer"] = answer
+			with open(answer_filename,"w") as write_file:
+				json.dump(answer_data, write_file)
+			return "Updated"
+			found = True
+			break
 
-	answer_dict = answer.dict()
-	answer_found = False
+	return "not found"
+@app.post('/questions/{course_id}') #reworked
+async def add_question(course_id : int ,questions : Questions,  user = Depends(get_current_user)):
+	course_id = int(course_id)
+	if not(user["admin"]):
+		return "Access denied"
+	question_dict = questions.dict()
 	question_found = False
-	for answer_idx, answer_list in enumerate(data["user_answers"]):
-		if answer_list['id'] == answer_dict['id']:
-			for question_list in data["questions"]:
-				if question_list['id'] == answer_dict['id']:
-					data["user_answers"][answer_idx] = answer_dict
-					answer_found = True
-					question_found = True
-					with open(json_filename,"w") as write_file:
-						json.dump(data, write_file)
-					return "Updated"
-	if not (answer_found): 
-		return "no Answer found"
+	for course in question_data:
+		if course["id"] == course_id:
+			question_data_this = course["question"]
+			break
+
+	for questions_list in question_data_this:
+		if questions_list['id'] == question_dict['id']:
+			item_found = True
+			return "Questions number "+str(question_dict['id'])+" exists."
 	if not (question_found):
-		return "no Answer found"
+		question_data_this.append(question_dict)
+		with open(question_filename,"w") as write_file:
+			json.dump(question_data, write_file)
+
+		return question_dict  
 	raise HTTPException(
-		status_code=404, detail=f'item not found'
+			status_code=404, detail=f'item not found'
 	)
+
+@app.delete('/questions/{course_id}/{questions_id}') #reworked
+async def delete_questions(course_id : int, questions_id : int,user = Depends(get_current_user)):
+	course_id = int(course_id)
+	questions_id = int(questions_id)
+	if not(user["admin"]):
+		return "Access denied"
+	for course in question_data:
+		if course["id"] == course_id:
+			question_data_this = course["question"]
+	for questions_idx, questions_item in enumerate(question_data_this):
+		if questions_item['id'] == questions_id:
+			question_data_this.pop(questions_idx)
+			with open(question_filename,"w") as write_file:
+				json.dump(question_data, write_file)
+			return "updated"
+	raise HTTPException(
+			status_code=404, detail=f'item not found'
+	)
+
+@app.post('/course')
+async def add_course(first_question : Questions,user = Depends(get_current_user)):
+	course_id = 0
+	if not(user["admin"]):
+		return "Access denied"
+	for item in question_data:
+		if course_id <= item['id']:
+			course_id = item['id'] + 1
+	insert_course = {"id" : course_id, "question" : [{"id" : first_question.id, "question" : first_question.question, "correct_answer" : first_question.correct_answer}]}
+	question_data.append(insert_course)
+	with open(question_filename,"w") as write_file:
+		json.dump(question_data, write_file)
+	return "success"
